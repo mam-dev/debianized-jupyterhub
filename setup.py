a2ba360b7ddf21e1ee0158ee880e953a8f588b3e
jhermann <jh@web.de>
2018-07-18 17:59:39 +0200

    setup: add Altair (needs Python 3.5.3+)

--- setup.py
+++ setup.py
@@ -121,6 +121,7 @@ project = dict(
         # extensions
         'seaborn==0.8.1',
         'bokeh==0.13.0', 'psutil==5.4.6',
+        'altair==2.1.0', 'vega==1.3.0', 'vega_datasets==0.5.0',  # needs Python 3.5.3+
     ],
     extras_require=dict(
         spark=['pyspark==2.3.1', 'pyspark-flame==0.2.4'],
