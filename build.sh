9bf4a88f7723ba169f6a696ff57a4a42f3978c6b
jhermann <jh@web.de>
2018-10-02 15:20:48 +0200

    build: remove old staging dir code

--- build.sh
+++ build.sh
@@ -10,25 +10,18 @@ image="${1:?You MUST provide a docker image name}"; shift
 dist_id=${image%%:*}
 codename=${image#*:}
 pypi_name="$(./setup.py --name)"
+pypi_version="$(./setup.py --version)"
 pkgname="$(dh_listpackages)"
-pkgversion="$(dpkg-parsechangelog -S Version)"
 tag=$pypi_name-$dist_id-$codename
-staging_dir="build/staging"
-
-# Prepare staging area
-rm -rf $staging_dir 2>/dev/null || true
-mkdir -p $staging_dir
-git ls-files >build/git-files
-rm -f build/${pkgname}?*${pkgversion}*.*
-test ! -f .npmrc || echo .npmrc >>build/git-files
-tar -c --files-from build/git-files | tar -C $staging_dir -x
 
 # Build in Docker container, save results, and show package info
+rm -f dist/${pkgname}?*${pypi_version//./?}*${codename}*.*
 docker build --tag $tag \
     --build-arg "DIST_ID=$dist_id" \
     --build-arg "CODENAME=$codename" \
     --build-arg "PKGNAME=$pkgname" \
     -f Dockerfile.build \
-    "$@" $staging_dir
+    "$@" .
 mkdir -p dist
-docker run --rm $tag tar -C /dpkg -c . | tar -C dist -xv
+docker run --rm $tag tar -C /dpkg -c . | tar -C dist -x
+ls -lh dist/${pkgname}?*${pypi_version//./?}*${codename}*.*
