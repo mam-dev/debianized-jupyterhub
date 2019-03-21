72c9e110ff227bec883ebe6c1176fc632045dbfb
jhermann <jh@web.de>
2019-03-21 12:39:56 +0100

    setup / vizjs: adding plotly_express

--- setup.py
+++ setup.py
@@ -157,7 +157,7 @@ project = dict(
             'holoviews[recommended]==1.11.3',
         ],
         vizjs=[
-            'plotly==3.7.0', 'cufflinks==0.14.6',
+            'plotly==3.7.0', 'plotly_express==0.1', 'cufflinks==0.14.6',
             'bokeh==1.0.4', 'psutil==5.6.1', 'chartify==2.6.0',
             'altair==2.4.1', 'vega==1.4.0', 'vega_datasets==0.7.0',  # needs Python 3.5.3+
             'selenium==3.141.0', 'chromedriver-binary==2.46.0', 'phantomjs-binary==2.1.3',
