89636ae51b93c327c21c11e2c06eaaf6e8a9a531
jhermann <jh@web.de>
2020-02-21 17:05:59 +0100

    build: support for Buster / Stretch (use their builtin Python)

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@
 
 set -e
 
+# If you chnage this, you MUST also change "debian/control" and "debina/rules"
+PYTHON_MINOR="3.8"  # Deadsnakes version on Ubuntu
+
+
 # Get build platform as 1st argument, and collect project metadata
 image="${1:?You MUST provide a docker image name}"; shift
 dist_id=${image%%:*}
@@ -13,6 +17,7 @@ pypi_name="$(./setup.py --name)"
 pypi_version="$(./setup.py --version)"
 pkgname="$(dh_listpackages)"
 tag=$pypi_name-$dist_id-$codename
+staging="$pypi_name-$pypi_version"
 
 build_opts=(
     -f Dockerfile.build
@@ -23,12 +28,28 @@ build_opts=(
 )
 
 if test "$dist_id" = "ubuntu"; then
-    build_opts+=( --build-arg "PYVERSION=3.8" )
+    build_opts+=( --build-arg "PYVERSION=$PYTHON_MINOR" )
 fi
 
+
+# Create Docker staging directory and apply environment changes
+echo "*** Creating staging directory (sdist)"
+echo "*** Ignore warnings regarding non-matching filters..."
+python3 ./setup.py -q sdist -k
+if test "$dist_id" = "debian"; then
+    sed -i -e "s/python$PYTHON_MINOR/python3/g" "$staging/debian/control"
+fi
+
+mkdir -p build
+rm -rf build/docker.staging 2>/dev/null
+mv "$staging" build/docker.staging
+
+
 # Build in Docker container, save results, and show package info
+echo
+echo "*** Building DEB package (takes a while)"
 rm -f dist/${pkgname}?*${pypi_version//./?}*${codename}*.*
-docker build "${build_opts[@]}" "$@" .
+docker build "${build_opts[@]}" "$@" build/docker.staging
 mkdir -p dist
 docker run --rm $tag tar -C /dpkg -c . | tar -C dist -x
 ls -lh dist/${pkgname}?*${pypi_version//./?}*${codename}*.*
